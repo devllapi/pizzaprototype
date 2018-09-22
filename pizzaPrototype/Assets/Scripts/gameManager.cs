c05af0a6ae71f45f53ac1aa54398e3a92d71288f
Unknown <mad758@nyu.edu>
2018-09-22 16:48:11 -0400

    Added bread spinning script
    We're gonna get the bread to spin (also theres some ui fixes)

--- pizzaPrototype/Assets/Scripts/gameManager.cs
+++ pizzaPrototype/Assets/Scripts/gameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 public class gameManager : MonoBehaviour {
     public Text combatText;
     public LemonSqueezeMinigame squeezeScript;
+
+    public Button button1;
+    public Button button2;
+    public Button button3;
 	// Use this for initialization
 	void Start () {
 		
@@ -23,10 +27,12 @@ public class gameManager : MonoBehaviour {
     }
     public void BreadBash()
     {
-        combatText.text = "Bread Button Pressed";
+        combatText.text = "Spell not learned!";
+        button2.interactable=false;
     }
     public void OreganoOreganized()
     {
-        combatText.text = "Oregano Button Pressed";
+        combatText.text = "Spell not learned!";
+        button3.interactable = false;
     }
 }
