d5cd4549fdcb80ece20c965aa5235714139befc7
Michael Cleary <swim2see@gmail.com>
2018-09-29 19:18:09 -0400

    Fiddled around with the timing game buttons
    did not succeed

--- pizzaPrototype/Assets/Scripts/OreganoStun.cs
+++ pizzaPrototype/Assets/Scripts/OreganoStun.cs
@@ -10,12 +10,26 @@ public class OreganoStun : MonoBehaviour
 	public float maxTime = 100f;
 	public float curTime = 0f;
 
+	
+	
+	private bool buttonOneActive;
+	private bool buttonOneDead;
+	private bool buttonOneResult;
+	
+	private bool buttonTwoActive;
+	private bool buttonTwoDead;
+	private bool buttonTwoResult;
+
+	private bool buttonThreeActive;
+	private bool buttonThreeDead;
+	private bool buttonThreeResult;
 
 	public Image Bar;
 
 	// Use this for initialization
 	void Start ()
 	{
+		//Time.timeScale = .1f; 
 		curTime = 0f; 
 		//at the start of script, 
 		InvokeRepeating("increaseTime", 0f, 0.03f);
@@ -23,6 +37,30 @@ public class OreganoStun : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+		//the amount of time it takes for the bar to reach the first button
+		if (curTime >= 27f)
+		{
+			buttonOneActive = true;
+			Debug.Log(buttonOneActive);
+		} else if (curTime <= 44f)
+		{
+			buttonOneActive = false;
+			Debug.Log(buttonOneActive);
+		}
+
+		if (buttonOneActive == true)
+		{
+			if (Input.GetButtonDown("joystick button 1"))
+			{
+				buttonOneResult = true;
+				Debug.Log("success");
+			}
+			else
+			{
+				buttonOneResult = false;
+				Debug.Log("fail");
+			}
+		}
 
 	}
 
@@ -39,4 +77,12 @@ public class OreganoStun : MonoBehaviour
 		Bar.fillAmount = theTime;
 	}
 
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "oCircle")
+		{
+			//buttonActive = true;
+			Debug.Log("active");
+		}
+	}
 }
