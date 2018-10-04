b79e1258eeb7a5e946d5dd1f8a0787e421fa4e43
Unknown <mad758@nyu.edu>
2018-10-04 15:44:08 -0400

    Charles Saves Our Project
    There's so much to unpack in this

---
+++ pizzaPrototype/Assets/Scripts/enemyAIMaster.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class enemyAIMaster : MonoBehaviour {
+    public int enemyHealth;
+    int whoseTurn;
+
+    public enemy[] enemyList;
+    public int playerSelect;
+    public GameObject enemyPrefab;
+
+    // Use this for initialization
+    void Start () {
+
+        generateEnemies(1, 3);
+
+        enemyList[playerSelect].health -= 40;
+        if(playerSelect >= enemyList.Length)
+        {
+            playerSelect = 0;
+        }else if (playerSelect < 0)
+        {
+            playerSelect = enemyList.Length - 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (whoseTurn == 0)
+        {
+            //players turn
+        }
+        if (whoseTurn == 1)
+        {
+            //enemies turn
+            for (int i = 0; i < enemyList.Length; i++)
+            {
+                if(Random.Range(0,1) > 0.5f)
+                {
+                    enemyList[i].attack();
+                }
+                else
+                {
+                    enemyList[i].defend();
+                }
+               
+            }
+        }
+        
+	}
+
+    
+
+    //This function will generate a series of random enemies (according to params)
+    //and set them to the EnemyList
+    public void generateEnemies(int min, int max)
+    {
+        //Determines the Length of the List
+        int num = (int)Random.Range(min, max);
+        enemyList = new enemy[num];
+
+        //Spawns list.length number of enemies
+        for(int i = 0; i < num; i++)
+        {
+            //Grabs relevant component of Chef
+            enemyList[i] = Instantiate(enemyPrefab, this.gameObject.transform).GetComponent<enemyChef>();
+
+            //Sets enemy instance to be child of this object
+            enemyList[i].gameObject.transform.SetParent(gameObject.transform, true);
+        }
+    }
+}
---
+++ pizzaPrototype/Assets/Scripts/enemyChef.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyChef : enemy
+{
+    void Start()
+    {
+        health = 100;
+        mana = 50;
+    }
+
+    public override void attack()
+    {
+        heal();
+    }
+
+    public override void defend()
+    {
+
+    }
+
+    public void heal()
+    {
+
+    }
+}
