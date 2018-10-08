72a56d12906ac420b3ee970adfb84ca6f83afe6a
Unknown <mad758@nyu.edu>
2018-10-08 18:38:17 -0400

    The Beginning of Reorganizing Code

--- pizzaPrototype/Assets/Scripts/enemyAIMaster.cs
+++ pizzaPrototype/Assets/Scripts/enemyAIMaster.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class enemyAIMaster : MonoBehaviour {
-    
+public class enemyAIMaster : MonoBehaviour
+{
+
     int whoseTurn;//determines who is performing actions
 
     public enemy[] enemyList;//generates an array based on the enemy superclass
@@ -13,97 +14,117 @@ public class enemyAIMaster : MonoBehaviour {
 
     public LemonSqueezeMinigame tenderizerScript;
     public potSpinning stirSauce;
-    
+    public OreganoCircle oCircles;
+    public OreganoStun oBar;
+    public GameObject oreganoHolder;
+
+    public GameObject hideUI;
 
     public Image enemyHealthBar;
 
+    public bool minigameActive;
+
+    public enum CombatState
+    {
+        enemySelect,
+        spellSelect,
+        minigame,
+        enemyTurn
+    }
+
+    public CombatState combatState;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         generateEnemies(1, 3);
         whoseTurn = 0;
         //enemyList[playerSelect].health -= 40; <this is how we affect an enemies health>
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (whoseTurn == 0)//players turn
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (combatState)
         {
-            canISelectThings = true;
-            print(canISelectThings);
-            if (canISelectThings == true)
-            {
-                gameManager.gm.hideUI.SetActive(false);
+            case CombatState.enemySelect:
                 selectTarget();
-                enemyList[playerSelect].glowObj.SetActive(true);
-            }
-            //players turn
-            //enemyList[playerSelect].takeDamage(10);
-            if (tenderizerScript.gameState == 0)
-            {
+                print("enemySelect");
+                break;
+            case CombatState.spellSelect:
+                SpellSelection();
+                print("spellSelect");
+                break;
+            case CombatState.minigame:
+                Minigames();
+                print("minigame");
+                Debug.Log(tenderizerScript.gameState);
 
-                if (tenderizerScript.squeezeCount > 20 && tenderizerScript.squeezeCount < 30)
+                if (tenderizerScript.gameState == 0 && gameManager.gm.tenderizerGame == true)
                 {
-                    enemyList[playerSelect].takeDamage(20);
-                    
-                }
-                else if (tenderizerScript.squeezeCount > 30)
-                {
-                    enemyList[playerSelect].takeDamage(40);
-                }
-                else if (tenderizerScript.squeezeCount < 25 && tenderizerScript.squeezeCount > 0)
-                {
-                    enemyList[playerSelect].takeDamage(10);
-                }
-                tenderizerScript.squeezeCount = 0;
-            }
-            if (stirSauce.gameState == 0)
-            {
+                   
+                    if (tenderizerScript.squeezeCount > 20 && tenderizerScript.squeezeCount < 30)
+                    {
+                        enemyList[playerSelect].takeDamage(20);
 
-                if (stirSauce.fullRotation > 10 && stirSauce.fullRotation < 15)
-                {
-                    enemyList[playerSelect].takeDamage(20);
+                    }
+                    else if (tenderizerScript.squeezeCount > 30)
+                    {
+                        enemyList[playerSelect].takeDamage(40);
+                    }
+                    else if (tenderizerScript.squeezeCount < 25 && tenderizerScript.squeezeCount > 0)
+                    {
+                        enemyList[playerSelect].takeDamage(10);
+                    }
+                    tenderizerScript.squeezeCount = 0;
+                    gameManager.gm.tenderizerGame = false;
+                    combatState = CombatState.enemyTurn;
+                    minigameActive = false;
                 }
-                else if (stirSauce.fullRotation > 15)
+
+                else if (stirSauce.gameState == 0 && gameManager.gm.stirringGame == true)
                 {
-                    enemyList[playerSelect].takeDamage(20);
+
+                    if (stirSauce.fullRotation > 10 && stirSauce.fullRotation < 15)
+                    {
+                        enemyList[playerSelect].takeDamage(20);
+                    }
+                    else if (stirSauce.fullRotation > 15)
+                    {
+                        enemyList[playerSelect].takeDamage(20);
+                    }
+                    else if (stirSauce.fullRotation < 10 && stirSauce.fullRotation > 0)
+                    {
+                        enemyList[playerSelect].takeDamage(10);
+                    }
+                    stirSauce.fullRotation = 0;
+                    gameManager.gm.stirringGame = false;
+                    combatState = CombatState.enemyTurn;
                 }
-                else if (stirSauce.fullRotation < 10 && stirSauce.fullRotation > 0)
-                {
-                    enemyList[playerSelect].takeDamage(10);
-                }
-                stirSauce.fullRotation = 0;
-            }
-            whoseTurn = 1;
+
+                break;
+            case CombatState.enemyTurn:
+                print("enemyTurn");
+                EnemyAttackPattern();
+                break;
         }
-        if (whoseTurn == 1)
-        {
-            //enemies turn
-            for (int i = 0; i < enemyList.Length; i++)
-            {
-                if(Random.Range(0,1) > 0.5f)
-                {
-                    enemyList[i].attack();
-                }
-                else
-                {
-                    enemyList[i].defend();
-                }
-               
-            }
-        }
-        whoseTurn = 0;
-        
-	}
+
+
+
+    }
 
     public void selectTarget()
     {
+        hideUI.SetActive(false);
+
         playerSelect = 0;
-        if (Input.GetAxisRaw("Horizontal")>0)
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
         {
             playerSelect += 1;
-        }else if (Input.GetAxis("Horizontal") < 0)
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
         {
             playerSelect -= 1;
         }
@@ -119,10 +140,54 @@ public class enemyAIMaster : MonoBehaviour {
 
         if (Input.GetButtonDown("Submit"))
         {
-            canISelectThings = false;
-            gameManager.gm.reactiveUI();
+            combatState = CombatState.spellSelect;
+            print(combatState);
         }
     }
+    public void SpellSelection()
+    {
+        hideUI.SetActive(true);
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            combatState = CombatState.minigame;
+        }
+    }
+
+    public void Minigames()
+    {
+        hideUI.SetActive(false);
+
+        if (!minigameActive)
+        {
+            if (gameManager.gm.tenderizerGame == true)
+            {
+                tenderizerScript.gameState = 1;
+                minigameActive = true;
+            }
+            if (gameManager.gm.stirringGame == true)
+            {
+                stirSauce.gameState = 1;
+                minigameActive = true;
+            }       
+        }
+    }
+    public void EnemyAttackPattern()
+    {
+        for (int i = 0; i < enemyList.Length; i++)
+        {
+            if (Random.Range(0, 1) > 0.5f)
+            {
+                enemyList[i].attack();
+            }
+            else
+            {
+                enemyList[i].defend();
+            }
+
+        }
+    }
+
 
     //This function will generate a series of random enemies (according to params)
     //and set them to the EnemyList
@@ -133,10 +198,13 @@ public class enemyAIMaster : MonoBehaviour {
         enemyList = new enemy[num];
 
         //Spawns list.length number of enemies
-        for(int i = 0; i < num; i++)
+        for (int i = 0; i < num; i++)
         {
             //Grabs relevant component of Chef
-            enemyList[i] = Instantiate(enemyPrefab, this.gameObject.transform).GetComponent<enemyChef>();
+            Vector3 temp = new Vector3(i * 10f, 0f, 0f) + gameObject.transform.position;
+            enemyList[i] = Instantiate(enemyPrefab, temp, Quaternion.identity).GetComponent<enemyChef>();
+
+
 
             //Sets enemy instance to be child of this object
             enemyList[i].gameObject.transform.SetParent(gameObject.transform, true);
